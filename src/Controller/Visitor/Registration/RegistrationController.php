1e0a77742a679c4b2d61482e72d970e7de5a1d4b
Zakaryya  PAPA MOHAMED <zakaryya@macbook-pro-de-zakaryya.home>
2023-01-29 18:41:15 +0100

    Mise en place de l'inscription

---
+++ src/Controller/Visitor/Registration/RegistrationController.php
@@ -0,0 +1,120 @@
+<?php
+namespace App\Controller\Visitor\Registration;
+
+
+
+use DateInterval;
+use App\Entity\User;
+use DateTimeImmutable;
+use App\Form\RegistrationFormType;
+use App\Repository\UserRepository;
+use App\Service\SendEmailService;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Core\Exception\AccountExpiredException;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
+use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
+
+class RegistrationController extends AbstractController
+{
+    #[Route('/register', name: 'visitor.registration.register')]
+    public function register(Request $request,
+        UserPasswordHasherInterface $userPasswordHasher,
+        EntityManagerInterface $entityManager,
+        TokenGeneratorInterface $tokenGenerator,
+        SendEmailService $sendEmailService,
+      ): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // encode the plain password
+            $password_hashed = $userPasswordHasher->hashPassword($user, $form->get('password')->getData());
+            $user->setPassword($password_hashed);
+
+            // generate the token
+            $token_generated = $tokenGenerator->generateToken();
+            $user->setTokenForEmailVerification($token_generated);
+
+            // Generate the deadline for email validation
+            $now = new DateTimeImmutable('now');
+            $expires_at = $now->add(new DateInterval('P1D'));
+            $user->setExpiresAt($expires_at);
+
+
+            // Insert new table "user" in the database 
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            // send an email to user
+            $sendEmailService->send([
+                "sender_email"        => "testmonsaintjo@gmail.com",
+                "sender_name"         => "Zakaryya PM",
+                "recipient_email"     => $user->getEmail(),
+                "subject"             => "Vérification de votre compte sur le site MonSaintJo",
+                "html_template"       => "email/email_verification.html.twig",
+                "context"             => [
+                    "user_id"    => $user->getId(),
+                    "token"      => $user->getTokenForEmailVerification(),
+                    "expires_at" => $user->getExpiresAt()->format('d/m/Y H:i:s')
+                ]
+            ]);
+
+            return $this->redirectToRoute('visitor.welcome.index');
+        }
+
+        return $this->render('pages/visitor/registration/register.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/register/email-verif/{id<\d+>}/{token}', name: 'visitor.registration.email_verif')]
+    public function emailVerif(User $user, string $token, UserRepository $userRepository ) : Response 
+    {
+        if ( ! $user) {
+
+            throw new AccessDeniedException();
+        }
+
+        if ( $user->isIsVerified()) {
+
+            $this->addFlash('success', "Votre compte a déjà été vérifié ! Vous pouvez vous connecter.");
+            return $this->redirectToRoute('visitor.welcome.index');
+        }
+
+        if (empty($token) || empty($user->getTokenForEmailVerification()) || ($token !== $user->getTokenForEmailVerification())  ) {
+
+            throw new AccessDeniedException();
+        }
+
+        if (new \DateTimeImmutable('now') > $user->getExpiresAt()) {
+
+            $deadline = $user->getExpiresAt();
+            $userRepository->remove($user, true);
+
+            throw new CustomUserMessageAccountStatusException("Votre délai de vérification est dépassé depuis le : $deadline ! Veuillez vous réinscrire.");
+        }
+
+        $user->setIsVerified(true);
+        $user->setVerifiedAt(new \DateTimeImmutable('now'));
+
+        $user->setTokenForEmailVerification(null);
+        $user->setExpiresAt(null);
+
+        $userRepository->save($user, true);
+
+        $this->addFlash("message", "Votre compte a bien été vérifié ! Vous pouvez vous connecter.");
+        
+        return $this->redirectToRoute('visitor.welcome.index');
+    }
+
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,302 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[UniqueEntity(fields: ['email'], message: "Impossible de créer un compte avec cet email.")]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+
+
+    #[Assert\NotBlank(
+        message: "Le prénom est obligatoire."
+
+    )]
+    #[Assert\Length(
+        max: 255,
+        maxMessage: 'Le prénom doit contenir au maximum {{ limit }} caractères.',
+    )]
+    #[ORM\Column(length: 255)]
+    private ?string $firstName = null;
+
+
+
+    #[Assert\NotBlank(
+        message: "Le nom est obligatoire."
+
+    )]
+    #[Assert\Length(
+        max: 255,
+        maxMessage: 'Le nom doit contenir au maximum {{ limit }} caractères.',
+    )]
+    #[ORM\Column(length: 255)]
+    private ?string $lastName = null;
+
+
+
+
+    #[Assert\NotBlank(
+        message: "L'email est obligatoire."
+
+    )]
+    #[Assert\Length(
+        max: 180,
+        maxMessage: "L'email doit contenir au maximum {{ limit }} caractères.",
+    )]
+    #[Assert\Email(
+        message: "l'email {{ value }} n'est pas valide.",
+    )]
+    #[ORM\Column(length: 180, unique: true)]
+    private ?string $email = null;
+
+
+
+    #[ORM\Column]
+    private array $roles = [];
+
+
+
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    #[Assert\NotBlank(
+        message: "Le mot de passe est obligatoire."
+
+    )]
+    #[Assert\Length(
+        min: 12,
+        max: 4000,
+        minMessage: 'Le mot de passe doit contenir au minimum {{ limit }} caractères.',
+        maxMessage: 'Le mot de passe doit contenir au maximum {{ limit }} caractères.',
+    )]
+    #[Assert\Regex(
+        pattern: '#^(?=.*[a-zà-ÿ])(?=.*[A-ZÀ-Ỳ])(?=.*[0-9])(?=.*[^a-zà-ÿA-ZÀ-Ỳ0-9]).{12,4000}$#',
+        match: true,
+        message: 'Le mot de passe doit contenir au moins une lette minuscule, une lettre majuscule, un chiffre et caractère spécial.',
+    )]
+    #[Assert\NotCompromisedPassword(
+        message:"Ce mot de passe est facilement piratable ! Veuillez en choisir un autre. "
+    )]
+    private ?string $password = null;
+
+
+
+  
+    #[ORM\Column(options: array('default' => false))]
+    private ?bool $isVerified = null;
+
+
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $tokenForEmailVerification = null;
+
+
+
+    #[Gedmo\Timestampable(on: 'create')]
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $createdAt = null;
+
+
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $expiresAt = null;
+
+
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $verifiedAt = null;
+
+
+
+    #[Gedmo\Timestampable(on: 'update')]
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $updatedAt = null;
+
+    
+
+    public function __construct()
+    {
+        $this->isVerified = false;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function isIsVerified(): ?bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): self
+    {
+        $this->isVerified = $isVerified;
+
+        return $this;
+    }
+
+    public function getTokenForEmailVerification(): ?string
+    {
+        return $this->tokenForEmailVerification;
+    }
+
+    public function setTokenForEmailVerification(?string $tokenForEmailVerification): self
+    {
+        $this->tokenForEmailVerification = $tokenForEmailVerification;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(?\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getExpiresAt(): ?\DateTimeImmutable
+    {
+        return $this->expiresAt;
+    }
+
+    public function setExpiresAt(?\DateTimeImmutable $expiresAt): self
+    {
+        $this->expiresAt = $expiresAt;
+
+        return $this;
+    }
+
+    public function getVerifiedAt(): ?\DateTimeImmutable
+    {
+        return $this->verifiedAt;
+    }
+
+    public function setVerifiedAt(?\DateTimeImmutable $verifiedAt): self
+    {
+        $this->verifiedAt = $verifiedAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeImmutable $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+}
